d01d0b5ba4c4a4255a2cb9fecce5c0f754a4ecb2
Lars Krämer <kraemerlars01@gmail.com>
2024-11-25 21:22:04 +0100

    some styling

--- GuessTheNumber/src/app/game/gameplay/gameplay.component.ts
+++ GuessTheNumber/src/app/game/gameplay/gameplay.component.ts
@@ -13,11 +13,15 @@ export class GameplayComponent implements OnInit {
 
   difficulty: string = '';
   answer: string = 'test';
+  resultMessage: string = '';
   firstNumber: number = 0;
   lastNumber: number = 10;
   randomNumber: number = 0;
   guessedNumber: number = 0;
   showResult: boolean = false;
+  isCorrect: boolean = false;
+  isToLow: boolean = false;
+  isToHigh: boolean = false;
   
 
   ngOnInit(): void {
@@ -44,22 +48,45 @@ export class GameplayComponent implements OnInit {
     
     console.log(this.randomNumber)
 
-     if(this.difficulty == undefined) {
-      alert("Please select difficulty!")
-      return;
-     }
-
     if(this.guessedNumber == this.randomNumber) {
       console.log('Correct!');
       this.showResult = true;
+      this.isCorrect = true;
     } else if (this.guessedNumber < this.randomNumber) {
       console.log('To low!');
       this.showResult = true;
+      this.isCorrect = false;
+      this.isToLow = true;
+      this.resultMessage = "Wrong! Youre number is to low!";
     } else if(this.guessedNumber > this.randomNumber) {
       console.log('to high!');
       this.showResult = true;
-
-
+      this.isCorrect = false;
+      this.isToHigh = true;
+      this.resultMessage = "Wrong! Youre number is to high!";
     }
+
+    return "Invalid";
   }
+
+  applyHighlight() {
+    const element = document.getElementById("playAreaInput")!;
+    let count = 0;
+  
+    function addHighlight() {
+      if (count < 3) {
+        element.classList.add("highlight");
+  
+        setTimeout(() => {
+          element.classList.remove("highlight");
+          count++;
+          setTimeout(addHighlight, 100);
+        }, 1000);
+      }
+    }
+  
+    addHighlight();
+  }
+  
+  
 }
