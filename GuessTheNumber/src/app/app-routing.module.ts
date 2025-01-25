2aae1b1b8742d96e5eb39266b88b77754b47d458
Lars Krämer <kraemerlars01@gmail.com>
2025-01-25 18:54:09 +0100

    redriect to home directly

--- GuessTheNumber/src/app/app-routing.module.ts
+++ GuessTheNumber/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { GameComponent } from './game/game.component';
 import { TutorialComponent } from './tutorial/tutorial.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'play', component: GameComponent },
   { path: 'tutorial', component: TutorialComponent }
